fda571365e36c8826fcfab1332baec108ac4ebf8
AliIsmail10 <ali.mohamed.ali31199@gmail.com>
2025-04-30 19:24:31 +0300

    login

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../Shared/Services/Auth/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false;
+  constructor(private  AuthService: AuthService){
+
+    this.AuthService.isLoggedIn$.subscribe((status :any) => {
+      this.isLoggedIn = status;
+    });
+  }
+  logout() {
+     this.AuthService.logout();
+      this.isLoggedIn = false;
+
+
+}
+
 
 }
