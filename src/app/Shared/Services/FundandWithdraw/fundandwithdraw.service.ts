abee177cb3c39c8544a9e789bbba1b524f4565e9
MostafSayed1 <mostafasayed7410@gmail.com>
2025-05-01 01:12:43 +0300

    update

--- src/app/Shared/Services/FundandWithdraw/fundandwithdraw.service.ts
+++ src/app/Shared/Services/FundandWithdraw/fundandwithdraw.service.ts
@@ -23,11 +23,18 @@ export class FundandwithdrawService {
     return this._HttpClient.put<FundsCard>(`${this.apiUrl}/UpdateFundsCard/${id}`, fund);
   }
 
-  addfund(fund:FundsCard):Observable<FundsCard> {
+  cardfund(fund:FundsCard):Observable<FundsCard> {
     return this._HttpClient.post<FundsCard>(`${this.apiUrl}/AddFundsCard`, fund);
   }
+  cardwithdraw(fund:FundsCard):Observable<FundsCard> {
+    return this._HttpClient.post<FundsCard>(`${this.apiUrl}/WithdrawCard`, fund);
+  }
 
-
-
+  stripefund(amount:number,email:string):Observable<any> {
+    return this._HttpClient.get<any>(`${this.apiUrl}/AddFundsStripe?money=${amount}&email=${email}`);
+  }
+  stripewithdraw(amount:number,email:string):Observable<any> {
+    return this._HttpClient.get<any>(`${this.apiUrl}/StripeWithdraw?money=${amount}&email=${email}`);
+  }
 
 }
