0ff01d5ce44a3b28d1ea01b957c7fbdf88693037
MahmoudHossam00 <mahmodmohammed345@gmail.com>
2025-05-04 19:24:56 +0200

    addingskills

--- src/app/Shared/Services/Skill/skill.service.ts
+++ src/app/Shared/Services/Skill/skill.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Environment } from '../../../base/environment';
 import { Observable } from 'rxjs';
-import { FreelancerSkill, nonrecommendedSkill, Skill } from '../../Interfaces/Skill';
+import { FreelancerSkill, nonrecommendedSkill, Skill, skillcreatedto } from '../../Interfaces/Skill';
 import { UserSkill } from '../../Interfaces/UserSkill';
 @Injectable({
   providedIn: 'root'
@@ -20,10 +20,10 @@ export class SkillService {
     return this._HttpClient.get(`${this.apiUrl}Skills/${id}`);
   }
 
-  addSkill(skill: Skill): Observable<Skill> {
+  addSkill(skill: skillcreatedto): Observable<Skill> {
     return this._HttpClient.post<Skill>(`${this.apiUrl}Skills`, skill);
   }
-  updateSkill(id: number, skill: Skill): Observable<Skill> {
+  updateSkill(id: number, skill: skillcreatedto): Observable<Skill> {
     return this._HttpClient.put<Skill>(`${this.apiUrl}Skills/${id}`, skill);
   }
   deleteSkill(id: number): Observable<void> {
